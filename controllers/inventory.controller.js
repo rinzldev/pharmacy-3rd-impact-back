cca7404ea0cc4650d79baa5c3311d01eeb8d7765
Oswaldo Yanez <oswaldo.business31@gmail.com>
2023-02-09 16:53:54 -0400

    fixing

--- controllers/inventory.controller.js
+++ controllers/inventory.controller.js
@@ -63,19 +63,19 @@ async function createInventory(req, res) {
 
 async function getInventoryByFilter(req, res) {
   try {
-    //const inventories = await sequilize.query(`SELECT * FROM public."Inventories" as I inner join public."Offices" as O on I."SID" = O."SID"`)
+    const inventories = await sequilize.query(`SELECT * FROM public."Inventories" as I inner join public."Offices" as O on I."SID" = O."SID"`)
     const pag = req.query.pag
     const size = req.query.size
-    const inventories = await db.sequelize.query(`
-      SELECT i."IID", o."code", o."name", m."code", 
-      l."name", m."name", m."presentation", i."quantity"
-      FROM public."Inventories" as i
-      inner join public."Offices" as o on i."SID" = o."SID"
-      inner join public."Medicines" as m on i."MID" = m."MID"
-      inner join public."Laboratories" as l on m."LID" = l."LID"
-      limit ${size}
-      offset ${pag}
-    `)
+    // const inventories = await db.sequelize.query(`
+    //   SELECT i."IID", o."code", o."name", m."code", 
+    //   l."name", m."name", m."presentation", i."quantity"
+    //   FROM public."Inventories" as i
+    //   inner join public."Offices" as o on i."SID" = o."SID"
+    //   inner join public."Medicines" as m on i."MID" = m."MID"
+    //   inner join public."Laboratories" as l on m."LID" = l."LID"
+    //   limit ${size}
+    //   offset ${pag}
+    // `)
 
     responses.makeResponsesOkData(res, inventories, "Success");
     //responses.makeResponsesOk(res, "Success");
@@ -177,32 +177,12 @@ async function deleteInventory(req, res) {
   }
 }
 
-async function getallwithJoin(req, res) {
-  try {
-    const inventoryData = req.body;
-    const inventories = await MInventory.findAll({
-      include: {
-        model: MOffice,
-        attributes: ["code"],
-        as: "o",
-      },
-      include: {
-        model: MMedicine,
-        attributes: ["code", "desc"],
-        as: "m",
-      },
-    });
-    responses.makeResponsesOkData(req, inventories, "Sucess");
-  } catch (e) {
-    responses.makeResponsesException(res, e);
-  }
-}
+
 module.exports = {
   createInventory,
   getAllInventories,
   getInventoryByFilter,
   getInventoryByID,
-  getallwithJoin,
   updateInventory,  
   deleteInventory,
   logicDeletInv,  
--- routes/inventory.route.js
+++ routes/inventory.route.js
@@ -4,7 +4,6 @@ const cInventory = require('../controllers/inventory.controller')
 
 router.get('/', cInventory.getAllInventories)
 router.get('/filter', cInventory.getInventoryByFilter)
-router.get('/withjoin', cInventory.getallwithJoin)
 router.get('/:id', cInventory.getInventoryByID)
 router.post('/', cInventory.createInventory)
 
